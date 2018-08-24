7b3697277d3076733d28fb8e7697f9a38402c467
Tiaan du Plessis <tiaanduplessis@hotmail.com>
2018-08-24 11:41:55 +0200

    Updates

--- src/index.js
+++ src/index.js
@@ -1,72 +1,77 @@
-function css(elem, rules = {}) {
+function css (elem, rules = {}) {
     Object.keys(rules).forEach(key => {
-        elem.style[key] = rules[key]
+      elem.style[key] = rules[key]
     })
-}
-
-function toast(
+  }
+  
+  function toast (
     content = '',
-    {
-        className = '',
-        duration = 3000,
-        target = 'body',
-        offset = 15,
-        styles = {},
-        selector = 'just-toasty'
-    } = {}
-) {
+    opts = {}
+  ) {
+    const {
+      className = '',
+      duration = 3000,
+      target = 'body',
+      offset = 15,
+      styles = {},
+      selector = 'toast',
+      cb = () => {}
+    } = typeof opts === 'number' ? {duration: opts} : opts
 
-    // TODO: Make position configurable
     const [vert, hor] = ['top', 'right']
     const targetElem = document.querySelector(target)
     const elem = document.createElement('div')
     elem.classList.add(selector)
     className && elem.classList.add(className)
     elem.innerHTML = content
-
+  
     css(
-        elem,
-        Object.assign(
-            {
-                [hor]: '15px',
-                opacity: 1,
-                padding: '1em 1.5em',
-                color: '#fff',
-                background: 'rgba(0,0,10,0.8)',
-                display: 'inline-block',
-                position: 'fixed',
-                borderRadius: '.2em',
-                top: '-100px',
-                transition: 'all 0.4s ease-out'
-            },
-            styles
-        )
+      elem,
+      Object.assign(
+        {
+          [hor]: '15px',
+          opacity: 1,
+          padding: '1em 1.5em',
+          zIndex: 9999,
+          color: '#fff',
+          background: 'rgba(0,0,10,0.8)',
+          display: 'inline-block',
+          position: 'fixed',
+          borderRadius: '.2em',
+          top: '-100px',
+          fontFamily: 'inherit',
+          transition: 'all 0.4s ease-out'
+        },
+        styles
+      )
     )
-
+  
     targetElem.insertBefore(elem, targetElem.firstChild)
-
+  
     let topOffset = offset
-
+  
     document.querySelectorAll(`.${selector}`).forEach(elem => {
-        const height = elem.clientHeight
-        css(elem, {
-            [vert]: topOffset + 'px'
-        })
-        topOffset += height + offset
+      const height = elem.clientHeight
+      css(elem, {
+        [vert]: topOffset + 'px'
+      })
+      topOffset += height + offset
     })
-
-    setTimeout(function() {
-        const width = elem.offsetWidth
-        css(elem, {
-            [hor]: '-' + width + 'px',
-            opacity: 0
-        })
-        setTimeout(() => {
-            elem.remove()
-        }, 1000)
+  
+    setTimeout(function () {
+      const width = elem.offsetWidth
+      css(elem, {
+        [hor]: '-' + width + 'px',
+        opacity: 0
+      })
+      setTimeout(() => {
+        elem.remove()
+        cb()
+      }, 1000)
     }, duration)
-
+  
     return elem
-}
-
-export default toast
+  }
+  
+  export default toast
+  
